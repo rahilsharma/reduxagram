774be8bf95844d205697f41edbbc78b43b253847
rahil sharma <liharsharma91092@gmail.com>
2016-10-14 19:47:01 +0530

    adding components folder

---
+++ client/components/Main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Rahil on 14-10-2016.
+ */
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,7 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+//importing css
+import css from './styles/style.styl';
+
+render(<p>Yo man</p>,document.getElementById('root'));
